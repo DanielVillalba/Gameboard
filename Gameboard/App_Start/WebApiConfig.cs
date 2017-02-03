7a0501f9d9dcc4c16de26e64e2ea9bdf98256a11
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-02-02 22:05:28 -0600

    Adding web api controller and return JSON mock data

--- Gameboard/App_Start/WebApiConfig.cs
+++ Gameboard/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace Gameboard
 {
@@ -10,6 +12,11 @@ namespace Gameboard
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+
+            // Use camel case for JSON data
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ Gameboard/Controllers/ProductsAPIController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Description;
+using Gameboard.Models;
+using Gameboard.Repositories;
+using Newtonsoft.Json;
+
+namespace Gameboard.Controllers
+{
+    public class ProductsAPIController : ApiController
+    {
+
+        //[DefaultConstructor]
+        public ProductsAPIController()
+        {
+        }
+
+        IRepository<Product, int> _Repository;
+        ProductsAPIController(IRepository<Product, int> repository)
+        {
+            _Repository = repository;
+        }
+
+        // retrieving all the products
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult Get()
+        {
+            // test data JSON object
+            Product test = new Product
+            {
+                Id = 1,
+                Name = "Test JSON object",
+                Description = "Test description",
+                Company = "Mock Data",
+                AgeRestriction = 15,
+                Price = 10.5m
+            };
+            //var test = _Repository.Get();    
+            return this.Ok(test);
+        }
+    }
+}
