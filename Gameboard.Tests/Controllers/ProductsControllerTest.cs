148a6ed0d52d579db838b68c0579444d396d9ab8
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-02-06 12:50:55 -0600

    Last cs before delivery

---
+++ Gameboard.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gameboard;
+using Gameboard.Controllers;
+using Gameboard.Repositories;
+using Gameboard.Models;
+
+namespace Gameboard.Tests.Controllers
+{
+    [TestClass]
+    public class ProductsControllerTest
+    {
+        IRepository<Product, int> repo = new ProductRepository();
+
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController(repo);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
