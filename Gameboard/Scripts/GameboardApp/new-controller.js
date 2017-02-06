e65ff4c6b2eb371f920935aef9b21ad1bcca2411
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-02-05 22:59:46 -0600

    Adding form to create new product

--- Gameboard/Scripts/GameboardApp/new-controller.js
+++ Gameboard/Scripts/GameboardApp/new-controller.js
@@ -1,56 +1,23 @@
 ﻿var mainApp = angular.module('GameboardApp');
 
 mainApp.controller('NewController', function ($scope, APIService) {
-    $scope.greeting = 'Adding new controller!';
+    $scope.greeting = 'Enter new products data!';
 
-    var newProduct = new Object();
-    newProduct.Id = 1;
-    newProduct.Name = "Added";
-    newProduct.Description = "Added des";
-    newProduct.AgeRestriction = 1;
-    newProduct.Company = "added company";
-    newProduct.Price = 12.45;
+    $scope.Id;
+    $scope.Name;
+    $scope.Description;
+    $scope.AgeRestriction;
+    $scope.Company;
+    $scope.Price;
 
-
-
-
-    newProducts(newProduct);
-
-    function newProducts(Data) {
-        var serviceCall = APIService.newProduct(Data);
-        //serviceCall.then(function (data) {
-        //    console.log(data);
-        //}, function (error) {
-        //    console.log('something went wrong !');
-        //})
+    $scope.saveProduct = function () {
+        var newProduct = new Object();
+        newProduct.Id = $scope.Id;
+        newProduct.Name = $scope.Name;
+        newProduct.Description = $scope.Description;
+        newProduct.AgeRestriction = $scope.AgeRestriction;
+        newProduct.Company = $scope.Company;
+        newProduct.Price = $scope.Price;
+        var serviceCall = APIService.newProduct(newProduct);
     }
-
 });
-
-
-
-//var TestCtrl = function ($scope, $http) {
-
-//    $scope.SendData = function (Data) {
-//        var GetAll = new Object();
-//        GetAll.FirstName = Data.firstName;
-//        GetAll.SecondName = Data.lastName;
-//        GetAll.SecondGet = new Object();
-//        GetAll.SecondGet.Mobile = Data.mobile;
-//        GetAll.SecondGet.EmailId = Data.email;
-//        $http({
-//            url: "NewRoute/firstCall",
-//            dataType: 'json',
-//            method: 'POST',
-//            data: GetAll,
-//            headers: {
-//                "Content-Type": "application/json"
-//            }
-//        }).success(function (response) {
-//            $scope.value = response;
-//        })
-//           .error(function (error) {
-//               alert(error);
-//           });
-//    }
-//};
